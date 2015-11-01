5f6e38485b29edda67b2e01fa906c8c5675cec2b
Roberto <rmartin.martin@gmail.com>
2015-11-01 18:20:03 -0500

    updating deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :linked_files, %w{config/database.yml config/secrets.yml}
 #dirs we want symlinking in shared
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :bundle_binstubs, nil
-#Rake::Task["deploy:compile_assets"].clear
+Rake::Task["deploy:compile_assets"].clear
 namespace :deploy do
 
   before :deploy, 'deploy:check_revision'
